6ec2359e2fe640b5f41f4604c92fd0071d2f51ad
xelav <xelav.rus@gmail.com>
2020-10-26 22:53:56 +0300

    Fix bibliography

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pandoc --filter pandoc-citeproc --bibliography=survey.bib -s survey.md -o survey.pdf --pdf-engine=xelatex --csl=gost.csl
+pandoc --citeproc --bibliography=survey.bib -s survey.md -o survey.pdf --pdf-engine=lualatex --csl=gost.csl
